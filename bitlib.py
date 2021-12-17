21a566d0053af069acc7e73651f2e6aac4e076ea
Gordon Bean <gbean@cs.byu.edu>
2021-12-16 20:44:28 -0700

    Add pyproject.toml

--- bitlib.py
+++ bitlib.py
@@ -51,13 +51,21 @@ _codes_to_colors = {
 _colors_to_codes = {v: k for k, v in _codes_to_colors.items()}
 
 
+MAX_STEP_COUNT = 1_000_000
+
+
 # Convention:
 # We'll have 0,0 be the origin
 # The position defines the X,Y coordinates
 class Bit:
+    _step_count: int
     world: np.array
     pos: np.array  # x and y
     orientation: int  # _orientations[orientation] => dx and dy
+
+    @staticmethod
+    def new_world(size_x, size_y):
+        return Bit(np.zeros((size_x, size_y)), (0, 0), 0)
 
     @staticmethod
     def load(filename: str):
@@ -89,8 +97,9 @@ class Bit:
 
     def __init__(self, world: np.ndarray, pos: np.array, orientation: int):
         self.world = world
-        self.pos = pos
+        self.pos = np.array(pos)
         self.orientation = orientation
+        self._step_count = 0
 
     def __repr__(self) -> str:
         """Present the bit information as a string"""
@@ -103,16 +112,19 @@ class Bit:
         orientation = self.orientation
         return f"{world_str}\n{pos_str}\n{orientation}\n"
 
+    def _step(self):
+        self._step_count += 1
+        if self._step_count > MAX_STEP_COUNT:
+            raise Exception("Bit has done too many things. Is he stuck in an infinite loop?")
+
     def save(self, filename: str):
-        """Save your bit world to a file"""
+        """Save your bit world to a text file"""
         with open(filename, 'wt') as f:
             f.write(repr(self))
+        print(f"Bit saved to {filename}")
 
-    def draw(self, filename=None) -> plt.Figure:
-        """Display the current state of the world"""
+    def _draw(self, ax):
         dims = self.world.shape
-        fig = plt.figure()
-        ax = fig.gca()
 
         # Draw squares
         for y in range(dims[1]):
@@ -136,12 +148,20 @@ class Bit:
         ax.set_ylim([0, dims[1]])
         ax.set_xticks(range(0, dims[0]))
         ax.set_yticks(range(0, dims[1]))
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
         ax.grid(True)
 
+    def draw(self, filename=None):
+        """Display the current state of the world"""
+        fig = plt.figure()
+        ax = fig.gca()
+        self._draw(ax)
         if filename:
             print("Saving bit world to " + filename)
             fig.savefig(filename)
-
+        else:
+            plt.show()
         fig.show()
 
     def _next_orientation(self, direction: Literal[1, 0, -1]) -> np.array:
@@ -157,19 +177,22 @@ class Bit:
         """If the direction is clear, move that way"""
         next_pos = self._get_next_pos()
         if not self._pos_in_bounds(next_pos):
-            raise MoveOutOfBoundsException(next_pos)
+            raise MoveOutOfBoundsException(f"Bit tried to move to {next_pos}, but that is out of bounds")
         elif self._get_color_at(next_pos) == BLACK:
             raise MoveBlockedByBlackException(next_pos)
         else:
             self.pos = next_pos
+            self._step()
 
     def left(self):
         """Turn the bit to the left"""
         self.orientation = self._next_orientation(1)
+        self._step()
 
     def right(self):
         """Turn the bit to the right"""
         self.orientation = self._next_orientation(-1)
+        self._step()
 
     def _get_color_at(self, pos):
         return self.world[pos[0], pos[1]]
@@ -184,12 +207,15 @@ class Bit:
 
         Black squares are not clear.
         """
+        self._step()
         return self._space_is_clear(self._get_next_pos())
 
     def left_clear(self) -> bool:
+        self._step()
         return self._space_is_clear(self._get_next_pos(1))
 
     def right_clear(self) -> bool:
+        self._step()
         return self._space_is_clear(self._get_next_pos(-1))
 
     def _paint(self, color: int):
@@ -197,14 +223,52 @@ class Bit:
 
     def erase(self):
         """Clear the current position"""
+        self._step()
         self._paint(EMPTY)
 
     def paint(self, color):
         """Color the current position with the specified color"""
+        self._step()
         if color not in _names_to_colors:
             raise Exception(f"Unrecognized color: {color}. Known colors are: {list(_names_to_colors.keys())}")
         self._paint(_names_to_colors[color])
 
     def get_color(self) -> str:
         """Return the color at the current position"""
+        self._step()
         return _colors_to_names[self._get_color_at(self.pos)]
+
+    def compare(self, other: 'Bit'):
+        """Compare this bit to another"""
+        if not self.world.shape == other.world.shape:
+            raise Exception(f"Cannot compare Bit worlds of different dimensions: {tuple(self.pos)} vs {tuple(other.pos)}")
+
+        fig, axs = plt.subplots(1, 3, figsize=(12, 4))
+
+        # Draw this
+        self._draw(axs[0])
+        axs[0].set_title("This Bit")
+
+        # Draw that
+        other._draw(axs[2])
+        axs[2].set_title("That Bit")
+
+        # Compare
+        equal = True
+        self._draw(axs[1])
+        for x in range(self.world.shape[0]):
+            for y in range(self.world.shape[1]):
+                if self._get_color_at((x, y)) != other._get_color_at((x, y)):
+                    equal = False
+                    # print(f"Color at {x},{y} does not match: {_colors_to_names[self._get_color_at((x, y))]} "
+                    #       f"vs {_colors_to_names[other._get_color_at((x, y))]}")
+                    axs[1].text(x + 0.6, y+0.6, "!",
+                                fontsize=16, weight='bold', bbox={'facecolor': 'yellow', 'alpha': 0.5})
+        axs[1].set_title("This vs That")
+        if self.pos[0] != other.pos[0] or self.pos[1] != other.pos[1]:
+            equal = False
+            print(f"Location of Bit does not match: {tuple(self.pos)} vs {tuple(other.pos)}")
+
+        plt.show()
+
+        return equal
