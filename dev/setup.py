6867d28b32562d11438029aea5bda0a248a1d0dc
Gordon Bean <gbean@cs.byu.edu>
2024-09-27 16:01:51 -0600

    working in-browser prototype

---
+++ dev/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='byubitweb',
+    version='0.1.0',
+    packages=['web_bit'],
+    py_modules=['web_bit'],
+    install_requires=[
+        'webcolors',
+    ]
+)
